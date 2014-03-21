46783b49d3f7984646d9497b26c376b17bee308a
Dharmit Shah <dharmit.dev@gmail.com>
2014-03-21 21:24:06 +0530

    Committing secure.py & README.md from old repo.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 secure
 ======
+
+Python program that keeps an eye on `/var/log/secure` and reports "Failed password" attempts.
+
+Features:
+--------
+* Checks the file `/var/log/secure` every minute for new occurrence of **Failed password** message(s).
+* If new attempt is found, it stores details in the SQLite database.
+* After adding details to the database, sends an e-mail to the configured address.
---
+++ secure.py
@@ -0,0 +1,38 @@
+# secure.py checks /var/log/secure for new occurrences of "Failed password" 
+# mesages and sends a mail for new break-in attempts on the system. I also
+# plan to add a feature to drop any traffic from an IP that has attempted to
+# break-in more than configured times.
+
+import os
+import sys
+import time
+
+# initially set to 0, MTIME keeps a track of the latest modification time for
+# /var/log/secure file
+
+MTIME = 0
+
+def scan_var_log_secure():
+    new_MTIME = os.path.getmtime("/var/log/secure")
+    if MTIME == new_MTIME:
+        time.sleep(60)
+        return
+    else:
+        # code to scan new occurrences.
+    return
+
+
+def main():
+    while 1:
+        scan_var_log_secure()
+    print MTIME
+
+
+if __name__ == "__main__":
+    try:
+        MTIME = os.path.getmtime("/var/log/secure")
+    except os.error as e:
+        print '/var/log/secure does not exist. Make sure the file exists and ' \
+                'try again later.'
+        sys.exit()
+    sys.exit(main())
