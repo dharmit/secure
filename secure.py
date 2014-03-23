4178dc6bc8e4b0b427a3a188629e5e0fbd6f8816
Dharmit Shah <dharmit.dev@gmail.com>
2014-03-23 17:23:07 +0530

    Modified secure.py to follow PEP8

--- secure.py
+++ secure.py
@@ -1,4 +1,4 @@
-# secure.py checks /var/log/secure for new occurrences of "Failed password" 
+# secure.py checks /var/log/secure for new occurrences of "Failed password"
 # mesages and sends a mail for new break-in attempts on the system. I also
 # plan to add a feature to drop any traffic from an IP that has attempted to
 # break-in more than configured times.
@@ -12,9 +12,9 @@ import time
 
 MTIME = 0
 
+
 def check_for_failed_password(list_of_readlines):
     l = list_of_readlines
-    
     for i in range(len(l)):
         #if has_failed_password_msg(l[i]) == True:
         if ' '.join(l[i].split(' ')[5:9]) == 'Failed password for root':
@@ -24,6 +24,7 @@ def check_for_failed_password(list_of_readlines):
             # stored events. Check only for new events.
         else:
             continue
+
 
 def scan_var_log_secure():
     global MTIME
@@ -38,8 +39,8 @@ def scan_var_log_secure():
                 check_for_failed_password(list_of_readlines)
         except IOError as e:
             print "You do not have enough permissions to access the file.\n" \
-                    "Run the program as sudo or root user and try again."
-            sys.exit()         
+                  "Run the program as sudo or root user and try again."
+            sys.exit()
 
         #print "new_MTIME = %f" % new_MTIME
         MTIME = new_MTIME
@@ -56,7 +57,7 @@ if __name__ == "__main__":
     try:
         MTIME = os.path.getmtime("/var/log/secure-20140323")
     except os.error as e:
-        print '/var/log/secure does not exist. Make sure the file exists and ' \
-                'try again later.'
+        print '/var/log/secure does not exist. Make sure the file exists and '\
+              'try again later.'
         sys.exit()
     sys.exit(main())
