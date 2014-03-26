a3759aa73089d4cff2126d894199adb103a29b8b
Dharmit Shah <dharmit.dev@gmail.com>
2014-03-26 19:53:09 +0530

    Changed exit status, changed condition from 'mtime = file' to check for file existence, defined database schema.

--- secure.py
+++ secure.py
@@ -12,7 +12,44 @@ import time
 # /var/log/secure file
 
 MTIME = 0
-DB = sqlite3.connect("secure.db")
+DB1 = None
+DB2 = None
+
+def database_exists():
+    return os.path.exists("var_log_secure.db") and os.path.exists("count.db")
+
+
+def create_database():
+    """We will create two databases. One for logging the details of attempt
+    like date & ip while another for keeping a track of number of attempts made
+    from a particular IP. In future we plan to drop all packets coming from an
+    IP that has made X (say, 5) attempts using firewall rules.
+    """
+    global DB
+    try:
+        DB1 = sqlite3.connect("var_log_secure.db")
+        DB2 = sqlite3.connect("count.db")
+
+        cursor1 = DB1.cursor()
+        cursor2 = DB2.cursor()
+        cursor1.execute("""CREATE TABLE IF NOT EXISTS
+                       attempts(id INTEGER PRIMARY KEY, day INTEGER,
+                       month INTEGER, year INTEGER, ip TEXT
+                       """)
+        cursor2.execute("""CREATE TABLE IF NOT EXISTS
+                        count(ip TEXT PRIMARY KEY, trials INTEGER
+                        """)
+        DB1.commit()
+        DB2.commit()
+    except Exception as e:
+        DB1.rollback()
+        DB2.rollback()
+        raise e
+    finally:
+        DB1.close()
+        DB2.close()
+
+
 
 def fetch_last_from_db():
     """ Code in this function will fetch the last entry in the db. This is
@@ -83,18 +120,20 @@ def scan_var_log_secure():
 
 
 def main():
-    global DB
+    if not database_exists():
+        create_database()
+    else:
+        # code to use the existing database.
+
     while 1:
         scan_var_log_secure()
         time.sleep(5)
-    DB.close()
 
 
 if __name__ == "__main__":
-    try:
-        MTIME = os.path.getmtime("/var/log/secure")
-    except os.error as e:
+    if os.path.exists("/var/log/secure"):
+        sys.exit(main())
+    else:
         print '/var/log/secure does not exist. Make sure the file exists and '\
               'try again later.'
-        sys.exit()
-    sys.exit(main())
+        sys.exit(1)
